2b876f2d437e1c46feda5c4f8bfed68c138d179d
Caden <jiangshen@users.noreply.github.com>
2017-12-16 03:13:45 -0500

    More clean up

--- app/src/main/java/com/example/caden/drawingtest/ImageManager.java
+++ app/src/main/java/com/example/caden/drawingtest/ImageManager.java
@@ -1,37 +1,14 @@
 package com.example.caden.drawingtest;
 
 /**
- * Created by caden on 10/13/17.
+ * Created by Caden on 10/13/17.
  */
 
 public class ImageManager {
 
     static byte[] imgData;
-    static byte[] drawnImg;
-    static int currID;
-
-    static int brushColor;
-
-    static String imgFileName;
 
     public static void setImage(byte[] data) {
         imgData = data;
     }
-
-    public static void setImageID(int id) {
-        currID = id;
-    }
-
-    public static void setBrushColor(int c) {
-        brushColor = c;
-    }
-
-    public static void setImgFileName(String s) {
-        imgFileName = s;
-    }
-
-    public static void setDrawnImg(byte[] data) {
-        drawnImg = data;
-    }
-
 }
